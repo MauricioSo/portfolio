6afbc79f0b68ef19da1edce2821fc30e8dd580de
unknown <rioplateado@gmail.com>
2020-01-16 01:24:59 -0300

    90% projects page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 const swup = new Swup(); 
+
+function init() { 
+    if(document.querySelector(".burguer")) { 
+const burguer = document.querySelector(".burguer");
+const list = document.querySelector(".list"); 
+
+
+burguer.addEventListener('click', () => {
+    list.classList.toggle("menu-active");
+    })
+
+list.addEventListener('click', () => {
+    list.classList.toggle("menu-active");
+})
+
+};
+}
+swup.on('contentReplaced', init); 
+
+const burguer = document.querySelector(".burguer");
+const list = document.querySelector(".list"); 
+
+
+burguer.addEventListener('click', () => {
+    list.classList.toggle("menu-active");
+    })
+
+list.addEventListener('click', () => {
+    list.classList.toggle("menu-active");
+})
